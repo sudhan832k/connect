a6214e1afbca9e22e94d27938e51880ec27d24e0
sudhan832k <selvasudhanm@gmail.com>
2025-01-04 14:59:34 +0530

    changes

--- frontend/src/friends/friends.component.ts
+++ frontend/src/friends/friends.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AuthorizedService } from '../service/authorized.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './friends.component.html',
   styleUrl: './friends.component.scss',
+  encapsulation: ViewEncapsulation.None, // Disables View Encapsulation
 })
 export class FriendsComponent {
+  public friendsList: any[] = [];
   constructor(private authService: AuthorizedService) {
     this.getAllFriends();
   }
   getAllFriends() {
     const res = this.authService.getAllFriends();
     res.subscribe((res) => {
-      console.log(res);
+      this.friendsList = res.result;
     });
   }
 }
